aaabc57d2124a158ed68e078770a158bec8c4776
vamlax <25211a05t5@gmail.com>
2026-08-02 14:01:53 +0530

    Time: 1 ms (99.95%), Space: 77.2 MB (49.16%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int i;
+        int n = nums.length;
+        int ms = Integer.MIN_VALUE;
+        int cs=0;
+        int count=0;
+        int max = nums[0];
+        for(i=0;i<n;i++){
+            if(nums[i]<0){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+
+        if(count==n){
+            for(i=1;i<n;i++){
+                if(nums[i]>max){
+                    max = nums[i];
+                }
+            }
+            return max;
+        }
+
+        else{ 
+            for(i=0;i<n;i++){
+                cs += nums[i];
+                if(cs<0){
+                    cs = 0;
+                }
+                ms = Math.max(cs,ms);
+            }
+            return ms;
+        }
+    }
+
+}
