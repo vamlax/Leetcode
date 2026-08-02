0ad996c7c12e3b7e495defa1461c042e25b16236
vamlax <25211a05t5@gmail.com>
2026-08-02 14:03:06 +0530

    Time: 1 ms (99.48%), Space: 47 MB (73.29%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int i,j;
+        int n = nums.length;
+        int ans[] = new int[n];
+        for(i = 0; i < n; i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+    return ans;
+    }
+}
